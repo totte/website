60cf4c3484d3b310cc8b5758d143ddd12f97e92a
totte <totte@tott.es>
2014-12-04 23:09:48 +0100

    Update cash values

--- home/campaign.php
+++ home/campaign.php
@@ -4,21 +4,21 @@
     <p>The only expenses we have at the moment are for maintaining the main server, through which we provide you Chakra and all the surrounding services. Due to the generous sponsorship by HOST1PLUS, we get a significant discount for the server, with the final cost being around €80 ($100) per month, which sums up to €960 ($1200) per year.</p>
     <p>This page is for all of you that wish in one way or another to donate money and help Chakra to be self-sustainable by its users. We can only state how deeply amazed we are by your commitment and greatly appreciate your support and love for this distribution! This is one more reason to work harder to reach our goals.</p>
     <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="9D8D3YXKD93U8"><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>
-    <h2>€0 raised of €960 goal</h2>
+    <h2>€35 ($43) raised of €960 ($1200) goal</h2>
 </div>
 <div class="col-md-4">
     <h3>January</h3>
     <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
-            100%
+        <div class="progress-bar" role="progressbar" aria-valuenow="44" aria-valuemin="0" aria-valuemax="100" style="width: 44%;">
+            44%
         </div>
     </div>
 </div>
 <div class="col-md-4">
     <h3>February</h3>
     <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">
-            80%
+        <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+            0%
         </div>
     </div>
 </div>
@@ -89,8 +89,8 @@
 <div class="col-md-4">
     <h3>November</h3>
     <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="width: 2%; min-width: 10%;">
-            2%
+        <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+            0%
         </div>
     </div>
 </div>
