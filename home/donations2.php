0a82166a0305618e773ff62b10339eff21daa834
totte <totte@tott.es>
2014-12-04 21:41:53 +0100

    Tweak CSS for donations2.php

--- home/donations2.php
+++ home/donations2.php
@@ -14,80 +14,6 @@
     <a class="serendipity_image_link" title="Chakra shop" href='http://www.zazzle.com/thechakraproject'><!-- s9ymdb:5 --><img class="serendipity_image_center" width="160" height="20"  src="/news/uploads/zazzleLogo_468x60.jpg" title="Chakra shop" alt="" /></a>
     <h2>Header explaining this part, 2014</h2>
     <p>Lorem ipsum dolor sit amet ad nauseum.</p>
-    <h3>January</h3>
-    <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
-            100%
-        </div>
-    </div>
-    <h3>February</h3>
-    <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">
-            80%
-        </div>
-    </div>
-    <h3>March</h3>
-    <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-            0%
-        </div>
-    </div>
-    <h3>April</h3>
-    <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-            0%
-        </div>
-    </div>
-    <h3>May</h3>
-    <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-            0%
-        </div>
-    </div>
-    <h3>June</h3>
-    <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-            0%
-        </div>
-    </div>
-    <h3>July</h3>
-    <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-            0%
-        </div>
-    </div>
-    <h3>August</h3>
-    <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-            0%
-        </div>
-    </div>
-    <h3>September</h3>
-    <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-            0%
-        </div>
-    </div>
-    <h3>October</h3>
-    <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-            0%
-        </div>
-    </div>
-    <h3>November</h3>
-    <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="width: 2%;">
-            2%
-        </div>
-    </div>
-    <h3>December</h3>
-    <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-            0%
-        </div>
-    </div>
-    <h2>Header explaining this part, 2014</h2>
-    <p>Lorem ipsum dolor sit amet ad nauseum.</p>
 </div>
 <div class="col-md-4">
     <h3>January</h3>
@@ -172,7 +98,7 @@
 <div class="col-md-4">
     <h3>November</h3>
     <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="width: 2%;">
+        <div class="progress-bar" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="width: 2%; min-width: 10%;">
             2%
         </div>
     </div>
