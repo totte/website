7b4e15e2dcdd829ef521366dfcc90b6525eb35c3
totte <totte@tott.es>
2014-12-05 14:01:17 +0100

    Rename campaign to 'Fundraising campaign for 2015'

--- home/campaign.php
+++ home/campaign.php
@@ -1,8 +1,8 @@
 <div class="col-md-12">
-    <h1>Chakra End of Year 2014 Fundraising</h1>
+    <h1>Fundraising campaign for 2015</h1>
     <p>Chakra is a completely community-based distribution and all our contributors and developers are working in their free time to provide it.</p>
-    <p>The only expenses we have at the moment are for maintaining the main server, through which we provide you Chakra and all the surrounding services. Due to the generous sponsorship by <a href="http://www.host1plus.com">Host1Plus</a>, we get a significant discount for the server, with the final cost being around €80 ($100) per month, which sums up to €960 ($1200) per year.</p>
-    <p>In other words, if 100 users donated €10, this campaign would be over and Chakra would ensure sustainability for 2015 based entirely on its own community.</p>
+    <p>The only expenses we have at the moment are for maintaining the main server, through which we provide you Chakra and all the surrounding services. Due to the generous sponsorship by <a href="http://www.host1plus.com">Host1Plus</a>, we get a significant discount for the server, with the final cost being around €84 ($100) per month, which sums up to €1000 ($1200) per year.</p>
+    <p>In other words, if for example 100 users donated €10, this campaign would be over and Chakra would ensure sustainability for 2015 based entirely on its own community.</p>
     <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="9D8D3YXKD93U8"><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>
     <h2>€35 ($43) raised of €960 ($1200) goal</h2>
 </div>
