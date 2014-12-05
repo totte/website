dd0afb2311a4eb6be960ce313bbb5c0fb31f5d31
totte <totte@tott.es>
2014-12-05 15:31:48 +0100

    Minor tweaks to phrasing and currency values

--- home/campaign.php
+++ home/campaign.php
@@ -2,9 +2,9 @@
     <h1>Fundraising campaign for 2015</h1>
     <p>Chakra is a completely community-based distribution and all our contributors and developers are working in their free time to provide it.</p>
     <p>The only expenses we have at the moment are for maintaining the main server, through which we provide you Chakra and all the surrounding services. Due to the generous sponsorship by <a href="http://www.host1plus.com">Host1Plus</a>, we get a significant discount for the server, with the final cost being around €84 ($100) per month, which sums up to €1000 ($1200) per year.</p>
-    <p>In other words, if for example 100 users donated €10, this campaign would be over and Chakra would ensure sustainability for 2015 based entirely on its own community.</p>
+    <p>But we need your help! Our goal is for Chakra to be funded by the people that use it, so it can be sustained in the long term and not be dependable on a person or company. If 100 users donated €10, this campaign would be over and Chakra would ensure sustainability for all of 2015 based entirely on its own community.</p>
     <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="9D8D3YXKD93U8"><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>
-    <h2>€35 ($43) raised of €960 ($1200) goal</h2>
+    <h2>€35 ($43) raised of €1000 ($1200) goal</h2>
 </div>
 <div class="col-md-4">
     <h3>January</h3>
