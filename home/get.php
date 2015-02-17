ff0f951cf6af014518e8344ccfce462dec9050ae
totte <totte@tott.es>
2015-02-17 12:54:01 +0100

    Add warning to get.php about netinstall being broken and offline installation issue

--- home/get.php
+++ home/get.php
@@ -3,6 +3,9 @@
 </div>
 <div class="col-md-8">
     <h3>Chakra 2014.11 "Euler"</h3>
+    <div class="alert alert-info" role="alert">
+        <strong>Heads up!</strong> Netinstall is not working and offline installation may fail if the machine is connected to the Internet.
+    </div>
     <br />
     <a class="btn btn-primary btn-lg" href="http://sourceforge.net/projects/chakra/files/2014.11/chakra-2014.11-euler-x86_64.iso/download">
         HTTP download
