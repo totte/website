afa11345f36ffc8c9f34e058e78110ad799f2754
totte <totte@tott.es>
2015-02-27 11:21:40 +0100

    Move everything in /home/ to /

--- netinstall.php
+++ netinstall.php
@@ -1,1 +1,5 @@
-home/netinstall.php
+<?PHP
+   // This file is used by the ISO net installer to find the conf file. In case 
+   // the gitorious url changes we only need to change this, instead of respinning
+   header("location: https://gitorious.org/chakra/tribe/raw/data/netinstall.conf"); 
+?> 
