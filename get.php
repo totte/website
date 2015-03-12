60268a2aba83613e4dcdbda5e92b98e98d30cf38
totte <totte@tott.es>
2015-03-12 22:24:10 +0100

    Edit get.php to account for the 2015.03 Euler release

--- get.php
+++ get.php
@@ -2,22 +2,21 @@
     <h2>Download</h2>
 </div>
 <div class="col-md-8">
-    <h3>Chakra 2014.11 "Euler"</h3>
-    <div class="alert alert-info" role="alert">
-        <strong>Heads up!</strong> Netinstall is not working and offline installation may fail if the machine is connected to the Internet.
-    </div>
-    <a class="btn btn-primary btn-lg" href="http://sourceforge.net/projects/chakra/files/2014.11/chakra-2014.11-euler-x86_64.iso/download">
-        HTTP download
+    <h3>Chakra 2015.03 "Euler"</h3>
+    <a class="btn btn-primary btn-lg" href="http://sourceforge.net/projects/chakra/files/2015.03/chakra-2015.03-euler-x86_64.iso/download">
+        HTTP download (SourceForge)
     </a>
-    <!-- <a class="btn btn-primary btn-lg" href="http://linuxtracker.org/download.php?id=6185cc7e4f35802de060f83e7e622ea3f42848d5&f=chakra-2014.09-euler-x86_64.iso.torrent&key=0"> -->
-    <a class="btn btn-primary btn-lg" href="http://rsync.chakraos.org/torrents/chakra-2014.11-euler-x86_64.iso.torrent">
+    <a class="btn btn-primary btn-lg" href="http://rsync.chakraos.org/releases/chakra-2015.03-euler-x86_64.iso">
+        HTTP download (chakraos.org)
+    </a>
+    <a class="btn btn-primary btn-lg" href="http://rsync.chakraos.org/torrents/chakra-2015.03-euler-x86_64.iso.torrent">
         BitTorrent download
     </a>
     <br />
     <br />
     <table class="table table-hover">
         <tr>
-            <td>Date</td><td>2014-11-11</td>
+            <td>Date</td><td>2015-03-09</td>
         </tr>
         <tr>
             <td>Size</td><td>1.8 GiB</td>
@@ -26,10 +25,10 @@
             <td>Architecture</td><td>x86-64</td>
         </tr>
         <tr>
-            <td>MD5 checksum</td><td>e1efe3b9c8320192639041b4e8f5bc9a</td>
+            <td>MD5 checksum</td><td>837dd07a9e2f70db6b50265ecb1c7283</td>
         </tr>
         <tr>
-            <td>SHA1 checksum</td><td>5ec31a2646afe719108a7e52d4aaf947e327f8b7</td>
+            <td>SHA1 checksum</td><td>db5c48aef0a3386603a71cc3cf31caa86322d3c3</td>
         </tr>
     </table>
 </div>
