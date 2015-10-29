ae873788e4689a021dbd8ea4ee60776f6f25db0e
totte <totte@tott.es>
2015-10-29 12:17:55 +0100

    Correct phrasing, spaces to tabs

--- netinstall.php
+++ netinstall.php
@@ -1,5 +1,5 @@
-<?PHP
-   // This file is used by the ISO net installer to find the conf file. In case 
-   // the gitorious url changes we only need to change this, instead of respinning
-   header("location: http://git.chakraos.org/tribe.git/blob_plain/HEAD:/data/netinstall.conf"); 
-?> 
+<?php
+	// This file is used by the ISO net installation mode to find its
+	// configuration file.
+	header("location: http://git.chakraos.org/tribe.git/blob_plain/HEAD:/data/netinstall.conf");
+?>
