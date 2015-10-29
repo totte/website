b6e3eb4cea7560b9ad9ffadad7675b236f7aa564
totte <totte@tott.es>
2015-10-29 12:29:57 +0100

    Convert spaces to tabs

--- get.php
+++ get.php
@@ -1,46 +1,46 @@
 <div class="col-md-12">
-    <h2>Download</h2>
+	<h2>Download</h2>
 </div>
 <div class="col-md-8">
-    <h3>Chakra 2015.03 "Euler"</h3>
-    <div class="alert alert-info" role="alert">
-        <strong>Heads up!</strong> <a href="https://chakraos.org/news/index.php?/archives/153-Restructure-of-repositories-system-will-require-manual-intervention.html">Important changes are happening in Chakra at the moment.</a> If you are downloading this ISO to install Chakra, we advise you to wait until our Plasma 5 release is out!
-    </div>
-    <a class="btn btn-primary btn-lg" href="http://sourceforge.net/projects/chakra/files/2015.03/chakra-2015.03-euler-x86_64.iso/download">
-        HTTP download (SourceForge)
-    </a>
-    <a class="btn btn-primary btn-lg" href="http://rsync.chakraos.org/torrents/chakra-2015.03-euler-x86_64.iso.torrent">
-        BitTorrent download
-    </a>
-    <br />
-    <br />
-    <table class="table table-hover">
-        <tr>
-            <td>Date</td><td>2015-03-09</td>
-        </tr>
-        <tr>
-            <td>Size</td><td>1.8 GiB</td>
-        </tr>
-        <tr>
-            <td>Architecture</td><td>x86-64</td>
-        </tr>
-        <tr>
-            <td>MD5 checksum</td><td>837dd07a9e2f70db6b50265ecb1c7283</td>
-        </tr>
-        <tr>
-            <td>SHA1 checksum</td><td>db5c48aef0a3386603a71cc3cf31caa86322d3c3</td>
-        </tr>
-    </table>
+	<h3>Chakra 2015.03 "Euler"</h3>
+	<div class="alert alert-info" role="alert">
+		<strong>Heads up!</strong> <a href="https://chakraos.org/news/index.php?/archives/153-Restructure-of-repositories-system-will-require-manual-intervention.html">Important changes are happening in Chakra at the moment.</a> If you are downloading this ISO to install Chakra, we advise you to wait until our Plasma 5 release is out!
+	</div>
+	<a class="btn btn-primary btn-lg" href="http://sourceforge.net/projects/chakra/files/2015.03/chakra-2015.03-euler-x86_64.iso/download">
+		HTTP download (SourceForge)
+	</a>
+	<a class="btn btn-primary btn-lg" href="http://rsync.chakraos.org/torrents/chakra-2015.03-euler-x86_64.iso.torrent">
+		BitTorrent download
+	</a>
+	<br />
+	<br />
+	<table class="table table-hover">
+		<tr>
+			<td>Date</td><td>2015-03-09</td>
+		</tr>
+		<tr>
+			<td>Size</td><td>1.8 GiB</td>
+		</tr>
+		<tr>
+			<td>Architecture</td><td>x86-64</td>
+		</tr>
+		<tr>
+			<td>MD5 checksum</td><td>837dd07a9e2f70db6b50265ecb1c7283</td>
+		</tr>
+		<tr>
+			<td>SHA1 checksum</td><td>db5c48aef0a3386603a71cc3cf31caa86322d3c3</td>
+		</tr>
+	</table>
 </div>
 <div class="col-md-4">
-    <img src="static/img/laptop.png" alt="wtflol" />
-    <h3>Get started</h3>
-    <ul>
-        <li><a href="https://chakraos.org/wiki/index
-        .php?title=Beginner%E2%80%99s_Guide">Beginner's Guide</a></li>
-        <li><a href="https://chakraos.org/wiki/index.php?title=Bootable_Medium_Creation">Creating a bootable USB or DVD</a></li>
-        <li><a href="https://chakraos.org/wiki/index
-        .php?title=Installation">Installing Chakra</a></li>
-        <li><a href="https://chakraos.org/wiki/index.php?title=Frequently_Asked_Questions">Frequently Asked Questions</a></li>
-    </ul>
+	<img src="static/img/laptop.png" alt="wtflol" />
+	<h3>Get started</h3>
+	<ul>
+		<li><a href="https://chakraos.org/wiki/index
+		.php?title=Beginner%E2%80%99s_Guide">Beginner's Guide</a></li>
+		<li><a href="https://chakraos.org/wiki/index.php?title=Bootable_Medium_Creation">Creating a bootable USB or DVD</a></li>
+		<li><a href="https://chakraos.org/wiki/index
+		.php?title=Installation">Installing Chakra</a></li>
+		<li><a href="https://chakraos.org/wiki/index.php?title=Frequently_Asked_Questions">Frequently Asked Questions</a></li>
+	</ul>
 </div>
