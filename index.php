0a77a36d33d5ef5582a023f7ef155e27f67731c6
totte <totte@tott.es>
2015-11-04 18:38:49 +0100

    Change our Facebook handle

--- index.php
+++ index.php
@@ -89,7 +89,7 @@ require_once 'functions.php';
 										border="0" alt="" width="22" height="22"
 										style="margin-top: -3px;"/></a></li>
 							<li>
-								<a href="https://www.facebook.com/theChakraProject"
+								<a href="https://www.facebook.com/chakraos"
 								   target="_blank"><img
 										src="static/img/facebook32.png"
 										border="0" alt="" width="22" height="22"
