11baf6da8ac001530e706204cacb84ab6cb4e938
totte <totte@tott.es>
2015-11-05 15:51:28 +0100

    Swap places for http and torrent download, add known issues section

--- get.php
+++ get.php
@@ -6,12 +6,12 @@
 	<div class="alert alert-info" role="alert">
 		<strong>Heads up!</strong> <a href="https://chakraos.org/news/index.php?/archives/153-Restructure-of-repositories-system-will-require-manual-intervention.html">Important changes are happening in Chakra at the moment.</a> If you are downloading this ISO to install Chakra, we advise you to wait until our Plasma 5 release is out!
 	</div>
-	<a class="btn btn-primary btn-lg" href="http://sourceforge.net/projects/chakra/files/2015.03/chakra-2015.03-euler-x86_64.iso/download">
-		HTTP download (SourceForge)
-	</a>
 	<a class="btn btn-primary btn-lg" href="http://rsync.chakraos.org/torrents/chakra-2015.03-euler-x86_64.iso.torrent">
-		BitTorrent download
+		BitTorrent download (recommended)
 	</a>
+    <a class="btn btn-primary btn-lg" href="http://sourceforge.net/projects/chakra/files/2015.03/chakra-2015.03-euler-x86_64.iso/download">
+        HTTP download (SourceForge)
+    </a>
 	<br />
 	<br />
 	<table class="table table-hover">
@@ -31,6 +31,14 @@
 			<td>SHA1 checksum</td><td>db5c48aef0a3386603a71cc3cf31caa86322d3c3</td>
 		</tr>
 	</table>
+    <h4>Known issues</h4>
+    <ul>
+        <li>The booting process might seem frozen for some seconds. This is due to the required elements loading, please be patient for some seconds to properly boot.
+        <li>If you are installing Chakra in <strong>VirtualBox</strong>, make sure to disable 3D acceleration under the  "Display" options if you experience issues with graphics.
+        <li>Chakra ISO images do not support <strong>unetbootin</strong> and DVDs need to be burned at a speed <strong>no higher than 4×</strong>. To create reliable installation media, please follow the instructions on our <a href="http://chakra-project.org/wiki/index.php?title=Bootable_Medium_Creation" title="Bootable Chakra media  ">wiki instructions</a>.
+        <li>There are limitations in detecting <strong>hybrid cards</strong>, so on some systems you might experience a black screen when booting from the ISO or rebooting after the installation. To workaround this just install using the free drivers and then <a href="https://chakraos.org/wiki/index.php?title=Switching_Between_Free_and_Non-free_Video-Drivers" title="Switch between free and nonfree drivers ">switch manually</a> to the proprietary ones.
+        <li>It is not possible yet to Install on <strong>RAID</strong>, <strong>LVM</strong>, <strong>LUKS</strong>, although you might find some workarounds on our forum if you want to get your hands dirty.
+    </ul>
 </div>
 <div class="col-md-4">
 	<img src="static/img/laptop.png" alt="wtflol" />
