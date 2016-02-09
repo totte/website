fe8962b35243f5585b170c33e995b190694b4f56
totte <totte@tott.es>
2016-02-09 23:49:21 +0100

    Fix CCR footer

--- ccr/template/footer.php
+++ ccr/template/footer.php
@@ -1,10 +1,26 @@
 
 	<!-- End of main content -->
 	</div>
-	<div id="foot">
-		Copyright &copy; 2006-<?php echo date('Y'); ?> <a href="mailto:chakra-devel@googlegroups.com">The Chakra Developers</a> | Sponsored by <a href="http://www.host1plus.com">Host1Plus</a> 
+	<div id="footer">
+		<script type="text/javascript">
+			var now = new Date();
+			document.write("Copyright &copy; 2006-" + now.getFullYear());
+		</script>
+		<a href="mailto:info@chakraos.org">Chakra</a>
 		&nbsp;
+		&middot;
+		&nbsp;
+		<a href="?legal">Legal information</a>
+		&nbsp;
+		&middot;
+		&nbsp;
+		<a href="?codeofconduct">Code of Conduct</a>
+		&nbsp;
+		&middot;
+		&nbsp;
+		Sponsored by <a href="http://www.host1plus.com">Host1Plus</a>
 	</div>
+	<!-- /#footer -->
 </div></div>
 </div></div>
 <br /><br />
