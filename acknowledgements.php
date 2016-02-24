c009b626c5fad5baed922f22452e9ab00598b0f9
totte <totte@tott.es>
2016-02-24 10:33:53 +0100

    Rename who.php to acknowledgements.php, split content to contributors.php

---
+++ acknowledgements.php
@@ -0,0 +1,262 @@
+<div class="col-md-12">
+	<h1>Acknowledgments</h1>
+	<h2>Former contributors</h2>
+	<?php
+		echo PrintFormerContributor("placeholder.png",
+			"",
+			"admiral0",
+			"",
+			"");
+		echo PrintFormerContributor("placeholder.png",
+			"",
+			"qote",
+			"",
+			"");
+		echo PrintFormerContributor("manutortosa.png",
+			"Manuel Tortosa",
+			"manutortosa",
+			"",
+			"");
+		echo PrintFormerContributor("djustice.png",
+			"Drake Justice",
+			"djustice",
+			"",
+			"");
+		echo PrintFormerContributor("abveritas.png",
+			"Anke Boersma",
+			"abveritas",
+			"",
+			"");
+		echo PrintFormerContributor("antonio.png",
+			"José Antonio Sánchez Reynaga",
+			"antonioJASR",
+			"",
+			"");
+		echo PrintFormerContributor("jmc.png",
+			"Daniele Cocca",
+			"jmc",
+			"",
+			"");
+		echo PrintFormerContributor("lisa.png",
+			"Lisa Vitolo",
+			"shainer",
+			"",
+			"");
+		echo PrintFormerContributor("codertux.png",
+			"Andrei Nistor",
+			"codertux",
+			"",
+			"");
+		echo PrintFormerContributor("surfed.png",
+			"Sascha Thier",
+			"surfed",
+			"",
+			"");
+		echo PrintFormerContributor("florent.png",
+			"Florent Nouvellon",
+			"florent",
+			"",
+			"");
+		echo PrintFormerContributor("dunkelstern.png",
+			"Johannes Schriewer",
+			"dunkelstern",
+			"",
+			"");
+		echo PrintFormerContributor("drf.png",
+			"Dario Freddi",
+			"drf",
+			"",
+			"");
+		echo PrintFormerContributor("jofko.png",
+			"Jozef Korencik",
+			"jofko",
+			"",
+			"");
+		echo PrintFormerContributor("sandsmark.png",
+			"Martin Sandsmark",
+			"sandsmark",
+			"",
+			"");
+		echo PrintFormerContributor("heller_barde.png",
+			"Philip Stark",
+			"heller_barde",
+			"",
+			"");
+		echo PrintFormerContributor("mistvieh.png",
+			"Dennis Brendel",
+			"mistvieh",
+			"",
+			"");
+		echo PrintFormerContributor("nachtwandler.png",
+			"Dominik Pieper",
+			"nachtwandler",
+			"",
+			"");
+		echo PrintFormerContributor("philm.png",
+			"Phil Müller",
+			"philm",
+			"",
+			"");
+		echo PrintFormerContributor("roozbeh.jpg",
+			"Roozbeh Shafiee",
+			"rosha",
+			"",
+			"");
+		echo PrintFormerContributor("funkyou.png",
+			"Jan Mette",
+			"funkyou",
+			"",
+			"");
+		echo PrintFormerContributor("placeholder.png",
+			"",
+			"pyrdracon",
+			"",
+			"");
+		echo PrintFormerContributor("placeholder.png",
+			"",
+			"malcer",
+			"",
+			"");
+	?>
+
+	<!-- SPONSORS -->
+	<h2>Sponsors</h2>
+	<a href="http://www.host1plus.com"><img src="static/img/logotype_host1plus.png" /></a>
+	<p>We'd like to thank <a href="http://www.host1plus.com">Host1Plus</a> who provides us with our main server at a great discount.</p>
+	<a href="http://www.jetbrains.com"><img src="static/img/logotype_jetbrains.png" /></a>
+	<p>We'd also like to thank <a href="http://www.jetbrains.com">JetBrains</a> who have licensed their Python IDE <a href="https://www.jetbrains.com/pycharm/">PyCharm Professional Edition</a> to our developers at no cost.</p>
+
+	<!-- MIRROR PROVIDERS -->
+	<a id="mirrorproviders"></a><h2>Mirror providers</h2>
+	<ul>
+		<li>
+			<a href="http://www.aarnet.edu.au/">
+				Australia's Academic and Research Network
+			</a>
+		</li>
+		<li>
+			<a href="http://www.c3sl.ufpr.br/">
+				C3SL - Centro de Computacao Cienti­fica e Software Livre
+			</a>
+		</li>
+		<li>
+			<a href="http://www.fis.unb.br/">
+				Universidade de Brasilia - Instituto de Fisica
+			</a>
+		</li>
+		<li>
+			<a href="http://www.ustc.edu.cn/">
+				University of Science and Technology of China
+			</a>
+		</li>
+		<li>
+			<a href="http://www.bjtu.edu.cn/">
+				Beijing Jiaotong University
+			</a>
+		</li>
+		<li>
+			<a href="http://www.tsinghua.edu.cn">
+				Tsinghua University
+			</a>
+		</li>
+		<li>
+			<a href="http://www.edatel.com.co/">
+				Edatel
+			</a>
+		</li>
+		<li>
+			<a href="http://www.lip6.fr/">
+				Laboratoire d'Informatique de l'université Paris 6
+			</a>
+		</li>
+		<li>
+			<a href="http://www.univ-lorraine.fr/">
+				Universite de Lorraine
+			</a>
+		</li>
+		<li>
+			<a href="http://www.selfnet.de/">
+				Selfnet e.V.- A non-profit student association
+			</a>
+		</li>
+		<li>
+			<a href="http://www.halifax.rwth-aachen.de/index.php/en/">
+				Wohnheim des Studentenwerk Aachen AoR
+			</a>
+		</li>
+		<li>
+			<a href="http://www.cc.uoc.gr/">
+				Υπολογιστικό Κέντρο - Σχολή Θετικών & Τεχνολογικών Επιστημών
+			</a>
+		</li>
+		<li>
+			<a href="http://www.garr.it/">
+				Consortium GARR - La Rete Italiana dell'Universita e della Ricerca
+			</a>
+		</li>
+		<li>
+			<a href="http://www.yandex.ru/">
+				Russian based search engine
+			</a>
+		</li>
+		<li>
+			<a href="http://www.puzzle.ch/">
+				Software-Entwicklung und Systems Engineering mit Open Source Technologien
+			</a>
+		</li>
+		<li>
+			<a href="http://free.nchc.org.tw/pmwiki/index.php?n=Main.HomePage">
+				The Free Software Lab at the NCHC
+			</a>
+		</li>
+		<li>
+			<a href="https://hostingxtreme.com/">
+				HostingXtreme - Leading provider of Web Hosting services
+			</a>
+		</li>
+		<li>
+			<a href="http://www.rit.edu/">
+				Rochester Institute of Technology
+			</a>
+		</li>
+		<li>
+			<a href="https://billing.dacentec.com/hostbill/">
+				Dacentec’s SSAE 16 SOC2 Certified Green Cloud Datacenter
+			</a>
+		</li>
+		<li>
+			<a href="http://ftp.yzu.edu.tw/">
+				Department of Computer Science and Engineering, Yuan Ze University, Taiwan, R.O.C
+			</a>
+		</li>
+		<li>
+			<a href="http://www.acc.umu.se/">
+				Academic Computer Club, Umeå University, Sweden
+			</a>
+		</li>
+	</ul>
+
+	<!-- DONORS -->
+	<h2>Donors</h2>
+
+	<!-- OTHERS -->
+	<h2>Others</h2>
+	<p>We would like to thank everyone who has enabled us to create Chakra, and/or contributed to it:</p>
+	<ul>
+		<li>The Arch Linux <a href="https://www.archlinux.org/people/developers/">developers</a> and community, for creating that awesome distribution</li>
+		<li>The Pacman developers, for creating the best package manager there is</li>
+		<li>The KDE developers, for creating the most powerful desktop environment there is</li>
+		<li>The KDEmod community, which was the starting point for Chakra and our tools</li>
+		<li>Michael Towers (gradgrind) for creating the awesome <a href="http://larch.berlios.de/">larch</a> LiveCD scripts</li>
+		<li>All our patient testers</li>
+		<li>Debian</li>
+		<li>Git</li>
+		<li>Gitolite</li>
+		<li>cgit</li>
+		<li>Python</li>
+		<li>Django</li>
+		<li>Xen</li>
+		<li>Freenode</li>
+		<li>SPI</li>
+	</ul>
+</div>
